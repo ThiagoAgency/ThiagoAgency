57b2d0ef8e137dc9e84f4c950bbf70184cab9b46
ThiagoAgency <151759754+ThiagoAgency@users.noreply.github.com>
2024-03-13 15:00:30 -0500

    Update ghost.js

--- src/lib/ghost.js
+++ src/lib/ghost.js
@@ -3,6 +3,6 @@ import GhostContentAPI from '@tryghost/content-api';
 // Crea una instancia de API con las credenciales del sitio
 export const ghostClient = new GhostContentAPI({
     url: 'https://thiagoagency.ghost.io', // Esto es la URL predeterminada si tu sitio se está ejecutando en un entorno local
-    key: '13beb18f407d388b1170be0d74',
+    key: '3a3f2ba7a67f62dfd29f904e06',
     version: 'v5.0',
 });
